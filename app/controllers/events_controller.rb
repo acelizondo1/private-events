212ca7fdeba3e649721386dd98d51e46064b741a
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-14 22:13:32 -0500

    Addin create event page with links on user show and navbar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,6 +19,16 @@ class EventsController < ApplicationController
 
     def create
         @event = current_user.created_events.build(event_params)
+
+        respond_to do |format|
+            if @event.save
+              format.html { redirect_to "/users/:current_user.id", notice: "Event was successfully created." }
+              format.json { render :show, status: :created, location: @event }
+            else
+              format.html { render :new, status: :unprocessable_entity }
+              format.json { render json: @event.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     def update 
@@ -32,7 +42,7 @@ class EventsController < ApplicationController
 
     private 
     def event_params
-        params.require(:event).allow(:name, :date, :location)
+        params.require(:event).permit(:name, :date, :location)
     end
 
     def set_event
