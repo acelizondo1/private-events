6c39b9f865a0b608fcf764257b1fec00241b4b49
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-22 08:06:55 -0500

    Changing db call to boolean type for user event page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
         @created_events = current_user.created_events
         @invited_events = current_user.invited_events.where.not("rsvp_status=?", 0)
 
-        @pending_event_invites = Event.joins(invites: :invitee).where("invites.invitee_id=? and invites.invite_status=? and invites.rsvp_status=?", current_user.id, 1, 2)
-        @declined_event_invites = Event.joins(invites: :invitee).where("invites.invitee_id=? and invites.invite_status=? and invites.rsvp_status=?", current_user.id, 1, 0)
+        @pending_event_invites = Event.joins(invites: :invitee).where("invites.invitee_id=? and invites.invite_status=? and invites.rsvp_status=?", current_user.id, true, 2)
+        @declined_event_invites = Event.joins(invites: :invitee).where("invites.invitee_id=? and invites.invite_status=? and invites.rsvp_status=?", current_user.id, true, 0)
     end
 end
