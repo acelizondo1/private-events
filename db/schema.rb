a5a51d4d34ef250ef32982bbe7c0202e79e8534b
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-22 07:13:33 -0500

    adjusting dev db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_18_225426) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
